96f7cb2a91c1d8b2d57ba7726074fb53ece32f48
EduardoVisconti <duduhsg@gmail.com>
2022-08-26 17:57:50 -0500

    Update script.js

--- Projeto06/script.js
+++ Projeto06/script.js
@@ -17,17 +17,24 @@ const secondsDisplay = document.querySelector('.seconds')
 // callback - passar função como argumento
 
 function countdown() {
-  setTimeout(function() {
+  setTimeout(function () {
     let seconds = Number(secondsDisplay.textContent)
+    let minutes = Number(minutesDisplay.textContent)
 
     if (seconds <= 0) {
       seconds = 60
+
+      minutesDisplay.textContent = String(minutes - 1).padStart(2, '0')
     }
 
-    secondsDisplay.textContent = seconds - 1
-    countdown() //rodar dnv
+    secondsDisplay.textContent = String(seconds - 1).padStart(2, '0') //quantos caracteres na nossa string e qual key vai preencher
 
-  }, 1000)//depois de quanto tempo queremos realizar a função, 1s
+    if (minutes <= 0) {
+      return
+    }
+
+    countdown() //rodar dnv
+  }, 1000) //depois de quanto tempo queremos realizar a função, 1s
 }
 
 buttonPlay.addEventListener('click', function () {
@@ -64,6 +71,6 @@ buttonSoundOff.addEventListener('click', function () {
 })
 
 buttonSet.addEventListener('click', function () {
-  minutes = prompt ('Quantos minutos?')
-  minutesDisplay.textContent = minutes
+  minutes = prompt('Quantos minutos?')
+  minutesDisplay.textContent = String(minutes).padStart(2, '0')
 })
