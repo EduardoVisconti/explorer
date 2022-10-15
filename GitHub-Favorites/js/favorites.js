5bb01264cb1ebe4eed93067df1fe92571d15f7c7
EduardoVisconti <duduhsg@gmail.com>
2022-10-15 11:42:02 -0500

    datas/users

--- GitHub-Favorites/js/favorites.js
+++ GitHub-Favorites/js/favorites.js
@@ -1,17 +1,4 @@
-export class GithubUser {
-  static search(username) { //metodo estatico
-    const endpoint = `https://api.github.com/users/${username}`
-
-    return fetch(endpoint)
-    .then(data => data.json())
-    .then(({login, name, public_repos, followers }) => ({
-      login,
-      name,
-      public_repos,
-      followers
-    }))
-  }
-}
+import { GithubUser } from "./GithubUser.js"
 
 //classe q vai conter a lógica dos dados
 //como os dados serão estruturados
@@ -27,26 +14,39 @@ export class Favorites {
     this.entries = JSON.parse(localStorage.getItem('@github-favorites:')) || []
   }
 
+  save() {
+    localStorage.setItem('@github-favorites:', JSON.stringify(this.entries)) //transforma o objeto em string
+  }
+
   async add(username) {
-    const user = await GithubUser.search(username)
+    try {
+      const userExists = this.entries.find(entry => entry.login === username)
+      if (userExists) {
+        throw new Error('Usuário já cadastrado')
+      }
 
-    if(user.login === undefined) {
-      throw new Error('Usuário não encontrado!')
+      const user = await GithubUser.search(username)
+
+      if (user.login === undefined) {
+        throw new Error('Usuário não encontrado!')
+      }
+
+      this.entries = [user, ...this.entries] //NOVO ARRAY. primeiro vai colocar o usuário, e todos os outros abaixo/espalhado
+      this.update()
+      this.save()
+    } catch (error) {
+      alert(error.message)
     }
-
-    this.entries = [user, ...this.entries] //NOVO ARRAY. primeiro vai colocar o usuário, e todos os outros abaixo/espalhado
-    this.update()
-  
-  } catch(error) {
-    alert(error.message)
   }
 
   delete(user) {
     const filteredEntries = this.entries.filter(
-      entry => entry.login !== user.login) //se for diferente, mantém
-    
+      entry => entry.login !== user.login
+    ) //se for diferente, mantém
+
     this.entries = filteredEntries //novo array
     this.update()
+    this.save()
   }
 }
 
@@ -77,10 +77,9 @@ export class FavoritesView extends Favorites {
       //para cada usuario
       const row = this.createRow()
 
-      row.querySelector(
-        '.user img'
-      ).src = `https://github.com/${user.login}.png`
+      row.querySelector('.user img').src = `https://github.com/${user.login}.png`
       row.querySelector('.user img').alt = `Imagem de ${user.name}`
+      row.querySelector('.user a').href = `https://github.com/${user.login}`
       row.querySelector('.user p').textContent = user.name
       row.querySelector('.user span').textContent = user.login
       row.querySelector('.repositories').textContent = user.public_repos
