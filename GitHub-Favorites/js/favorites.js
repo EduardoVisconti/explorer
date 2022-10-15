5f463d769205c1558897a31c4e315fc73a73a396
EduardoVisconti <duduhsg@gmail.com>
2022-10-14 19:21:06 -0500

    api github

---
+++ GitHub-Favorites/js/favorites.js
@@ -0,0 +1,129 @@
+export class GithubUser {
+  static search(username) { //metodo estatico
+    const endpoint = `https://api.github.com/users/${username}`
+
+    return fetch(endpoint)
+    .then(data => data.json())
+    .then(({login, name, public_repos, followers }) => ({
+      login,
+      name,
+      public_repos,
+      followers
+    }))
+  }
+}
+
+//classe q vai conter a lógica dos dados
+//como os dados serão estruturados
+export class Favorites {
+  constructor(root) {
+    this.root = document.querySelector(root)
+    this.load()
+
+    GithubUser.search('diego3g').then(user => console.log(user))
+  }
+
+  load() {
+    this.entries = JSON.parse(localStorage.getItem('@github-favorites:')) || []
+  }
+
+  async add(username) {
+    const user = await GithubUser.search(username)
+
+    if(user.login === undefined) {
+      throw new Error('Usuário não encontrado!')
+    }
+
+    this.entries = [user, ...this.entries] //NOVO ARRAY. primeiro vai colocar o usuário, e todos os outros abaixo/espalhado
+    this.update()
+  
+  } catch(error) {
+    alert(error.message)
+  }
+
+  delete(user) {
+    const filteredEntries = this.entries.filter(
+      entry => entry.login !== user.login) //se for diferente, mantém
+    
+    this.entries = filteredEntries //novo array
+    this.update()
+  }
+}
+
+//classe q vai criar a visualização e eventos do HTM L
+export class FavoritesView extends Favorites {
+  constructor(root) {
+    super(root) //div app
+
+    this.tbody = this.root.querySelector('table tbody')
+
+    this.update()
+    this.onadd()
+  }
+
+  onadd() {
+    const addButton = this.root.querySelector('.search button')
+    addButton.onclick = () => {
+      const { value } = this.root.querySelector('.search input')
+
+      this.add(value)
+    }
+  }
+
+  update() {
+    this.removeAllTr()
+
+    this.entries.forEach(user => {
+      //para cada usuario
+      const row = this.createRow()
+
+      row.querySelector(
+        '.user img'
+      ).src = `https://github.com/${user.login}.png`
+      row.querySelector('.user img').alt = `Imagem de ${user.name}`
+      row.querySelector('.user p').textContent = user.name
+      row.querySelector('.user span').textContent = user.login
+      row.querySelector('.repositories').textContent = user.public_repos
+      row.querySelector('.followers').textContent = user.followers
+
+      row.querySelector('.remove').onclick = () => {
+        const isOk = confirm('Tem certeza que deseja deletar essa linha?')
+        if (isOk) {
+          this.delete(user)
+        }
+      }
+
+      this.tbody.append(row)
+    })
+  }
+
+  createRow() {
+    const tr = document.createElement('tr')
+
+    tr.innerHTML = `
+            <td class="user">
+              <img
+                src="https://github.com/maykbrito.png"
+                alt="Imagem de maykbrito"
+              />
+              <a href="https://github.com/maykbrito" target="_blank">
+                <p>Mayk Brito</p>
+                <span>maykbrito</span>
+              </a>
+            </td>
+            <td class="repositories">76</td>
+            <td class="followers">9589</td>
+            <td>
+              <button class="remove">&times;</button>
+            </td>
+      
+    `
+    return tr //retorna o tr para usar em cada elemento
+  }
+
+  removeAllTr() {
+    this.tbody.querySelectorAll('tr').forEach(tr => {
+      tr.remove()
+    })
+  }
+}
