07b5db01dc17bb06c909342b47359f6d54e33729
Eduardo Visconti <duduhsg@gmail.com>
2023-06-23 12:48:44 -0400

    complete project

--- Stage05 IMC&Jogo/Jogo_Adivinha/main.js
+++ Stage05 IMC&Jogo/Jogo_Adivinha/main.js
@@ -3,12 +3,17 @@ const btnSubmit = document.querySelector('#btnSubmit')
 const btnRetry = document.querySelector('#btnRetry')
 const screen1 = document.querySelector('.screen1')
 const screen2 = document.querySelector('.screen2')
-const randomNumber = Math.round(Math.random() * 10)
+let randomNumber = Math.round(Math.random() * 10)
 let xAttempts = 1
 
 // Eventos
 btnSubmit.addEventListener('click', handleSubmitClick)
 btnRetry.addEventListener('click', handleRetryClick)
+document.addEventListener('keydown', function(e) {
+  if(e.key == 'Enter' && screen1.classList.contains('hide')) { //se o screen1 contém com o hide
+    handleRetryClick()
+  }
+})
 
 // Funções callback
 function handleSubmitClick(event) {
@@ -30,9 +35,12 @@ function handleSubmitClick(event) {
 function handleRetryClick() {
   toggleScreen()
   xAttempts = 1
+  randomNumber = Math.round(Math.random() * 10)
 }
 
 function toggleScreen() {
   screen1.classList.toggle('hide')
   screen2.classList.toggle('hide')
 }
+
+//refaturar a função > tem que ter número e apenas números entre 1-10
