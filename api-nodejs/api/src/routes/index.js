e47588f83a2334e4fd5112751d9c2e2ba1b82755
Eduardo Visconti <duduhsg@gmail.com>
2023-07-08 19:35:43 -0400

    index routes

--- api-nodejs/api/src/routes/index.js
+++ api-nodejs/api/src/routes/index.js
@@ -1,11 +1,15 @@
 //a missão deste index.js é reunir todas as rotas
 
-const {Router} = require("express")
+const { Router } = require('express')
 
-const usersRoutes = require("./users.routes") //grupo de rotas do usuário
+const usersRouter = require('./users.routes') //grupo de rotas do usuário
+const notesRouter = require('./notes.routes')
+const tagsRouter = require('./tags.routes')
 
 const routes = Router()
 
-routes.use('/users', usersRoutes) //toda vez que alguém acessar o /users será redirecionado ao usersRoute (que é o grupo de rotas do usuário ^^)
+routes.use('/users', usersRouter) //toda vez que alguém acessar o /users será redirecionado ao usersRoute (que é o grupo de rotas do usuário ^^)
+routes.use('/notes', notesRouter)
+routes.use('/tags', tagsRouter)
 
 module.exports = routes //que é o que contém todas as rotas da aplicação
