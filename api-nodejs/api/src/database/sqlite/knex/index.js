a3277f1dc8527b6c75bca9ed4df447aca7eb95f0
Eduardo Visconti <duduhsg@gmail.com>
2023-07-17 14:58:39 -0400

    creating knex

--- api-nodejs/api/src/database/sqlite/knex/index.js
+++ api-nodejs/api/src/database/sqlite/knex/index.js
@@ -1,4 +1,4 @@
-const config = require('../../../knexfile') //Pegando as configs do knex
+const config = require('../../../../knexfile') //Pegando as configs do knex
 const knex = require('knex') //Importando o knex
 
 const connection = knex(config.development) //Conexão knex > Dentro do config tem as configurações de desenvolvimento
