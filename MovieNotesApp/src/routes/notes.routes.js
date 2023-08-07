ba265c25ff4a939355e2729d39aa2ddba0fac1d9
Eduardo Visconti <duduhsg@gmail.com>
2023-08-07 02:13:53 -0400

    Create notes.routes.js

---
+++ MovieNotesApp/src/routes/notes.routes.js
@@ -0,0 +1,12 @@
+const { Router } = require('express')
+
+const NotesController = require('../controllers/NotesController.js')
+
+const notesRoutes = Router()
+
+const notesController = new NotesController()
+
+notesRoutes.post('/:user_id', notesController.create)
+notesRoutes.get('/:id', notesController.show)
+
+module.exports = notesRoutes
