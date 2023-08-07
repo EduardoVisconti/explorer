8ae252cda78d47d2325807af4b081886896868a6
Eduardo Visconti <duduhsg@gmail.com>
2023-08-07 02:13:44 -0400

    adding notes routes

--- MovieNotesApp/src/routes/index.js
+++ MovieNotesApp/src/routes/index.js
@@ -1,8 +1,10 @@
 const { Router } = require('express')
 
 const usersRoutes = require('./users.routes')
+const notesRoutes = require('./notes.routes')
 
 const routes = Router()
 routes.use('/users', usersRoutes)
+routes.use('/movie_notes', notesRoutes)
 
 module.exports = routes
