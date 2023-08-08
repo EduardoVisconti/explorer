7a8eed31f47291520360d611ae4980477e2dafe3
Eduardo Visconti <duduhsg@gmail.com>
2023-08-07 23:45:51 -0400

    update func index
    request.query thru insomnia
    connecting tables

--- MovieNotesApp/src/controllers/NotesController.js
+++ MovieNotesApp/src/controllers/NotesController.js
@@ -45,6 +45,46 @@ class NotesController {
 
     return response.json()
   }
+
+  async index(request, response) {
+    const { title, user_id, movie_tags } = request.query
+
+    let notes
+
+    if (movie_tags) {
+      const filterTags = movie_tags.split(',').map(tag => tag.trim())
+
+      notes = await knex('movie_tags')
+      .select([
+        'movie_notes.id',
+        'movie_notes.title',
+        'movie_notes.user_id',
+      ])
+      .where('movie_notes.user_id', user_id)
+      .whereLike('movie_notes.title', `%${title}%`)
+      .whereIn('name', filterTags)
+      .innerJoin('movie_notes', 'movie_notes.id', 'movie_tags.note_id')
+      .orderBy('movie_notes.title')
+
+    } else {
+      notes = await knex('movie_notes')
+        .where({ user_id })
+        .whereLike('title', `%${title}%`)
+        .orderBy('title')
+    }
+
+    const userTags = await knex('movie_tags').where({ user_id })
+    const notesWithTags = notes.map(note => {
+      const noteTags = userTags.filter(tag => tag.note_id === note.id)
+
+      return {
+        ...note,
+        tags: noteTags
+      }
+    })
+
+    return response.json(notesWithTags)
+  }
 }
 
 module.exports = NotesController
